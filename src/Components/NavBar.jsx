6fa38f74e6f9c58c28c6ba12dd3b9fef63e96ba4
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2025-07-14 12:39:20 -0700

    created manue for mobile screen

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,25 +5,59 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { RxCross1 } from "react-icons/rx";
 
 const NavBar = () => {
-  const [islogin, setLogin] = useState(true);
-
-  const login = () => {
-    setLogin(false);
+  const [navBar, setNavBar] = useState("");
+  const click = () => {
+    setNavBar(!navBar);
   };
 
-  const crossBar =() => {
-    setLogin(!islogin)
-  }
-  return (
-    <div className="w-full fixed top-0 left-0 right-0 shadow bg-[#111827] p-5  z-50">
-      <div className=" flex justify-between  items-center">
-        <div className="flex md:text-3xl ">
-          <SiJoplin className="text-4xl text-gray-500 " />
+ 
 
-          <h1 className="text-3xl font-semibold bg-gradient-nt">-temitope</h1>
+  return (
+    <>
+      <div className="w-full fixed top-0 left-0 right-0 shadow bg-[#111827] p-5  z-50">
+        <div className=" flex justify-between  items-center">
+          <div className="flex md:text-3xl ">
+            <SiJoplin className="text-4xl text-gray-500 " />
+
+            <h1 className="text-3xl font-semibold bg-gradient-nt">-temitope</h1>
+          </div>
+          <div className="">
+            <ul className="lg:flex hidden font-bold justify-between gap-10 capitalize text-xl cursor-pointer text-white">
+              <li className=" p-5">about</li>
+              <Link to="skills" smooth={true} duration={1000}>
+                <li className=" p-5">skills</li>
+              </Link>
+              <Link to="porfolio" smooth={true} duration={1000}>
+                <li className=" p-5">portfolio</li>
+              </Link>
+              <Link to="testimonials" smooth={true} duration={1000}>
+                <li className=" p-5">testimonials</li>
+              </Link>
+              <Link to="contact" smooth={true} duration={1000}>
+                <li className=" p-5">contacts</li>
+              </Link>
+            </ul>
+          </div>
+          <div class=" hidden:flex  items-center justify-center mt-10"></div>
+          <div class="lg:flex hidden group relative">
+            <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500  opacity-75 blur transition duration-500 group-hover:opacity-100"></div>
+            <button class=" relative items-center gap-4 rounded-lg bg-gray-900 cursor-pointer px-3 py-2 hover:text-gray-700 active:bg-gray-100 transition-all duration-700 font-bold text-white text-xl capitalize">
+              let's connect
+            </button>
+          </div>
+          <div className="lg:hidden text-2xl">
+            {navBar ? (
+              <RxHamburgerMenu onClick={click} />
+            ) : (
+              <RxCross1 onClick={click} />
+            )}
+          </div>
         </div>
-        <div className="">
-          <ul className="lg:flex hidden font-bold justify-between gap-10 capitalize text-xl cursor-pointer text-white">
+      </div>
+      {navBar &&
+      (
+        <nav>
+          <ul className="lg:hidden absolute right-0 top-20 bg-gray-800 font-bold opacity-95 capitalize w-full text-2xl  text-white text-center">
             <li className=" p-5">about</li>
             <Link to="skills" smooth={true} duration={1000}>
               <li className=" p-5">skills</li>
@@ -38,47 +72,9 @@ const NavBar = () => {
               <li className=" p-5">contacts</li>
             </Link>
           </ul>
-        </div>
-
-        <div class="hidden relative">
-          <div class="absolute -inset-1 rounded-lg bg-gradient00 opacity-75 blur"></div>
-          <button
-            onClick={login}
-            class="relative rounded-lg bg-gray-900 cursor-pointer px-3 py-5 hover:text-gray-700 active:bg-gray-100 transition-all duration-700 font-bold text-white text-xl capitalize"
-          >
-            let's connect
-          </button>
-        </div>
-      </div>
-
-     <div className="lg:hidden text-2xl z-10 ">
-        {islogin ? (
-          <RxHamburgerMenu onClick={login} className="flex lg:hidden" />
-        ) : (
-          <RxCross1 onClick={crossBar} className="flex lg:hidden" />
-        )}
-      </div>
-
-      <div className="">
-        <ul className="lg:hidden z-50 w-full text-center absolute  bg-gray-700 top-0 left-0 font-bold capitalize text-2xl text-white">
-           
-          <li className=" p-5">about</li>
-
-          <Link to="skills" smooth={true} duration={1000}>
-            <li className=" p-5">skills</li>
-          </Link>
-          <Link to="porfolio" smooth={true} duration={1000}>
-            <li className=" p-5">portfolio</li>
-          </Link>
-          <Link to="testimonials" smooth={true} duration={1000}>
-            <li className=" p-5">testimonials</li>
-          </Link>
-          <Link to="contact" smooth={true} duration={1000}>
-            <li className=" p-5">contacts</li>
-          </Link>
-        </ul>
-      </div>
-    </div>
+        </nav>
+      )}
+    </>
   );
 };
 
