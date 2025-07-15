e956246c98d1fdde10a145c3e563609e7ffe9576
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2025-07-14 22:57:08 -0700

    work on expertise page

--- src/Components/Expertise.jsx
+++ src/Components/Expertise.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 const Expertise = () => {
   return (
-    <div className="bg-[#111827] h-screen pt-10 max-sm:p-5 text-white">
+    <div className="bg-[#111827] h-screen pt-10 max-md:p-5 text-white">
       <div id="skills">
         <div>
-          <h1 className="text-center text-4xl font-bold mt-10 max-sm:mt-5">
+          <h1 className="text-center text-4xl  max-md:text-2xl font-bold mt-10 max-md:mt-15">
             Technical Skills
           </h1>
         </div>
 
-        <div className=" grid grid-cols-2 max-sm:grid-cols-1 gap-5 m-15 max-sm:m-5">
-          <div className="pb-5 max-sm:mt-5">
-            <h1 className="flex justify-between text-sm space-x-50 max-sm:space-30 mb-2">
+        <div className=" grid grid-cols-2 max-md:grid-cols-1 gap-5 max-md:gap-1 m-15 max-md:m-5 max-md:mt-2">
+          <div className="pb-5 max-md:pb-2 max-md:mt-2">
+            <h1 className="flex justify-between text-sm space-x-50 max-md:space-30 mb-2">
               React <span>70%</span>
             </h1>
             <div className=" h-3 bg-gray-800 rounded-2xl">
               <div className="w-[75%] h-full bg-gradient-to-r from-blue-500 to-purple-500"></div>
             </div>
           </div>
-          <div className="pb-5 max-sm:mt-5">
+          <div className="pb-5  max-md:pb-2 max-md:mt-2">
             <h1 className="flex justify-between text-sm space-x-50 mb-2">
               Tailwind <span>78%</span>
             </h1>
@@ -27,7 +27,7 @@ const Expertise = () => {
               <div className="w-[78%] h-full bg-gradient-to-r from-blue-500 to-purple-500"></div>
             </div>
           </div>
-          <div className="pb-5 max-sm:mt-5">
+          <div className="pb-5  max-md:pb-2 max-md:mt-2">
             <h1 className="flex justify-between text-sm space-x-50 mb-2">
               JavaScript <span>50%</span>
             </h1>
@@ -35,7 +35,7 @@ const Expertise = () => {
               <div className="w-[50%] h-full bg-gradient-to-r from-blue-500 to-purple-500"></div>
             </div>
           </div>
-          <div className="pb-5 max-sm:mt-5">
+          <div className="pb-5  max-md:pb-2 max-md:mt-2">
             <h1 className="flex justify-between text-sm space-x-50 mb-2">
               Modern CSS <span>78%</span>
             </h1>
@@ -43,7 +43,7 @@ const Expertise = () => {
               <div className="w-[78%] h-full bg-gradient-to-r from-blue-500 to-purple-500"></div>
             </div>
           </div>
-          <div className="pb-5 max-sm:mt-5">
+          <div className="pb-5  max-md:pb-2 max-md:mt-2">
             <h1 className="flex justify-between text-sm space-x-50 mb-2">
               UI/UX Design <span>70%</span>
             </h1>
@@ -51,7 +51,7 @@ const Expertise = () => {
               <div className="w-[70%] h-full bg-gradient-to-r from-blue-500 to-purple-500"></div>
             </div>
           </div>
-          <div className="pb-5 max-sm:mt-5">
+          <div className="pb-5  max-md:pb-2 max-md:mt-2">
             <h1 className="flex justify-between text-sm space-x-50 mb-2">
               Responsive Design <span>70%</span>
             </h1>
@@ -59,7 +59,7 @@ const Expertise = () => {
               <div className="w-[75%] h-full bg-gradient-to-r from-blue-500 to-purple-500"></div>
             </div>
           </div>
-          <div className="pb-5 max-sm:mt-5">
+          <div className="pb-5  max-md:pb-2 max-md:mt-2">
             <h1 className="flex justify-between text-sm space-x-50 mb-2">
               Web Animation <span>60%</span>
             </h1>
@@ -67,7 +67,7 @@ const Expertise = () => {
               <div className="w-[60%] h-full bg-gradient-to-r from-blue-500 to-purple-500"></div>
             </div>
           </div>
-          <div className="pb-5 max-sm:mt-5">
+          <div className="pb-5  max-md:pb-2 max-md:mt-2">
             <h1 className="flex justify-between text-sm space-x-50 mb-2">
               Git <span>80%</span>
             </h1>
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -8,19 +8,19 @@ const HeroSection = () => {
       <div className="items-center mt-15 pt-15 max-sm:pt-5 text-center">
         <div>
           <img
-            className="w-[200px] h-[200px] rounded-[50%] border-4 border-gray-600 mx-auto"
+            className="w-[200px] max-md:w-[150px] h-[200px] max-md:h-[150px] rounded-[50%] border-4 border-gray-600 mx-auto"
             src={myimage}
             alt="imagee"
           />
         </div>
-        <h1 className="leading-10 text-[1.7rem] font-bold text-white mt-5">
+        <h1 className="leading-10  text-[1.7rem]  max-md:text-[1.4rem] font-bold text-white mt-5">
           Akomolafe Temitope <br /> Joseph.
         </h1>
-        <h1 className="leading-10 text-[1.2rem] max-sm:text-[2rem] font-semibold text-white">
+        <h1 className="leading-10 max-md:leading-8 text-[1.2rem] max-md:text-[1.4rem]  font-semibold text-white">
           Fendend Developer
         </h1>
 
-        <p className="mt-5 max-sm:mt-2 font-bold text-l max-sm:text-xl max-sm:p-5 text-white">
+        <p className="mt-5 max-md:mt-2 font-bold text-l max-md:text-l max-md:p-2 text-white">
           I specialize in building clean, responsive, and high-performing
           websites. With expertise in React,
           <br /> Tailwind CSS, and JavaScript, I create seamless digital
@@ -32,7 +32,7 @@ const HeroSection = () => {
         <div class="flex  items-center justify-center mt-10 max-sm:mt-5 max-sm:p-5">
           <div class="group relative">
             <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-75 blur transition duration-500 group-hover:opacity-100"></div>
-            <button class="relative flex items-center gap-4 rounded-lg bg-gray-900 cursor-pointer px-7 max-sm:px-3 py-4 max-sm:py-3  hover:text-gray-700 active:bg-gray-100 transition-all duration-700 font-bold text-white text-xl capitalize max-sm:text-sm">
+            <button class="relative flex items-center gap-4 rounded-lg bg-gray-900 cursor-pointer px-7 max-md:px-2 py-4 max-md:py-3  hover:text-gray-700 active:bg-gray-100 transition-all duration-700 font-bold text-white text-xl capitalize max-md:text-sm">
               let's work together to bring your ideas to life
             </button>
           </div>
