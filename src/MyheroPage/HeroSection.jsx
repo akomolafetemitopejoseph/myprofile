97513917e926056534ca0eb00f7add8c67526612
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2025-07-27 05:58:57 -0700

    Added typeAnimation to my herosection

--- src/MyheroPage/HeroSection.jsx
+++ src/MyheroPage/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import myimage from "../assets/mypics.jpg";
 import Btn from "./btn";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
@@ -16,15 +17,29 @@ const HeroSection = () => {
             alt="imagee"
           />
         </div>
-        <h1 className="leading-10  text-[1.7rem]  max-md:text-[1.4rem] max-sm:text-[2rem] font-bold text-white mt-5">
+        <h1 className="leading-10  text-[1.7rem]  max-md:text-[1.4rem] max-sm:text-[1.5rem] font-bold text-white mt-5">
           Akomolafe Temitope <br /> Joseph.
         </h1>
         <div>
           <div className="w-50 m-auto mt-3 h-1 bg-gray-700"></div>
-          <h1 className="leading-10 max-md:leading-8 text-[1.2rem] max-md:text-[1.4rem] max-sm:text[2rem]  font-semibold text-white max-md:mt-3">
+          {/* <h1 className="leading-10 max-md:leading-8 text-[1.2rem] max-md:text-[1.4rem] max-sm:text[2rem]  font-semibold text-white max-md:mt-3">
             Fendend Developer
-          </h1>
-           <div className="w-50 m-auto mt-3 h-1 bg-gray-700"></div>
+          </h1> */}
+          <div>
+            <TypeAnimation
+              sequence={[
+                "I am a frontend developer",
+                1000,
+                "I specialize in building clean, responsive,  and high-performing websites. ",
+                1000,
+              ]}
+              wrapper="span"
+              speed={30}
+              className="leading-tight max-md:leading-8 text-[1.2rem] max-md:text-[20px] max-sm:text[1rem]  font-semibold text-white max-md:mt-3"
+              repeat={Infinity}
+            />
+          </div>
+          <div className="w-50 m-auto mt-3 h-1 bg-gray-700"></div>
         </div>
 
         <p className="mt-5  max-md:mt-4 font-bold text-l max-sm:text-xl max-md:text-l max-md:p-2 text-white">
